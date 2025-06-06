3ec0fd14ae80df38b1530aec1a9c495cbd9acf0a
Doncham <doncham98@gmail.com>
2025-06-07 00:14:55 +0900

    인증 방식 - 쿠키를 통한 인증방식

--- backend/src/main/java/org/juniortown/backend/config/AuthResolver.java
+++ backend/src/main/java/org/juniortown/backend/config/AuthResolver.java
@@ -13,9 +13,13 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 public class AuthResolver implements HandlerMethodArgumentResolver {
 	private final SessionRepository sessionRepository;
 	@Override
@@ -26,10 +30,18 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-		String accessToken = webRequest.getHeader("Authorization");
-		if (accessToken == null || accessToken.equals("")) {
+		HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+		if (servletRequest == null) {
+			log.error("servletRequest null");
 			throw new Unauthorized();
 		}
+		Cookie[] cookies = servletRequest.getCookies();
+		if (cookies.length == 0) {
+			log.error("쿠키 없음");
+			throw new Unauthorized();
+		}
+		String accessToken = cookies[0].getValue();
+
 		Session session = sessionRepository.findByAccessToken(accessToken)
 			.orElseThrow(Unauthorized::new);
 
